1cf16359fadd6453247fbc8b4383b73adee3961b
Sulakshika-Gunasekara <161403829+Sulakshika-Gunasekara@users.noreply.github.com>
2025-02-02 13:02:49 +0530

    Remove problematic file

--- src/components/__tests__/ProfilePage.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import MyWallet from "../pages/MyWallet";
-
-test("renders user's balance", () => {
-
-  const balanceElement = screen.getByText(/\$10\.50/);
-  expect(balanceElement).toBeInTheDocument();
-});
--- src/pages/FindEmptyDock.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Layout from "../components/Layout"; // Import Layout component
-
-const FindEmptyDock = () => {
-    return <h1>This is the Find Empty Dock Page</h1>;
-};
-
-export default FindEmptyDock;
